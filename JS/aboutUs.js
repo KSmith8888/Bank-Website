ac39b79d5c33213527e8a16fdbe3c3ddf146b703
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-08-18 17:37:15 -0700

    Create aboutUs.js

---
+++ JS/aboutUs.js
@@ -0,0 +1,23 @@
+const board1 = document.getElementById('board1');
+const board2 = document.getElementById('board2');
+const board3 = document.getElementById('board3');
+const board4 = document.getElementById('board4');
+const board5 = document.getElementById('board5');
+let members = [board1, board2, board3, board4, board5];
+
+for(let i = 0; i < 5; i++) {
+fetch(`https://jsonplaceholder.typicode.com/users/${i + 1}`)
+.then((response) => {
+    if(!response.ok) {
+        throw new Error(`Network error: ${response.status}`)
+    } else {
+        return response.json();
+    }
+})
+.then((data) => {
+    members[i].innerText = `${data.name} --- ${data.email} --- ${data.phone} --- ${data.website}`;
+})
+.catch((err) => {
+    console.log(err);
+});
+}
