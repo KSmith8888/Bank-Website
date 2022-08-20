16906be51083d15f8a881a4eba824dfd7bc3275d
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-08-19 18:35:33 -0700

    Update script.js
    Updated the event listeners for closing the "tabs" so that when a tab is closed it's radio button is unchecked and the background color of the ad section is reset.

--- JS/script.js
+++ JS/script.js
@@ -26,34 +26,44 @@ tabSection.addEventListener('click', () => {
     tab4.style.backgroundColor = 'rgb(228, 216, 216)';
     tab5.style.backgroundColor = 'rgb(228, 216, 216)';
     if(tabBtn1.checked) {
-        tab1.style.backgroundColor = 'rgb(184, 168, 168)';
+        tab1.style.backgroundColor = 'rgb(190, 189, 189)';
         ads.style.backgroundColor = 'lightgreen';
     } else if(tabBtn2.checked) {
-        tab2.style.backgroundColor = 'rgb(184, 168, 168)';
+        tab2.style.backgroundColor = 'rgb(190, 189, 189)';
         ads.style.backgroundColor = 'lightblue';
     } else if(tabBtn3.checked) {
-        tab3.style.backgroundColor = 'rgb(184, 168, 168)';
+        tab3.style.backgroundColor = 'rgb(190, 189, 189)';
         ads.style.backgroundColor = 'lightcoral';
     } else if(tabBtn4.checked) {
-        tab4.style.backgroundColor = 'rgb(184, 168, 168)';
+        tab4.style.backgroundColor = 'rgb(190, 189, 189)';
         ads.style.backgroundColor = 'yellow';
     } else if(tabBtn5.checked) {
-        tab5.style.backgroundColor = 'rgb(184, 168, 168)';
+        tab5.style.backgroundColor = 'rgb(190, 189, 189)';
         ads.style.backgroundColor = 'grey';
     }
     tabClose1.addEventListener('click', () => {
+        tabBtn1.checked = false;
+        ads.style.backgroundColor = 'rgb(228, 216, 216)';
         tab1.remove();
     })
     tabClose2.addEventListener('click', () => {
+        tabBtn2.checked = false;
+        ads.style.backgroundColor = 'rgb(228, 216, 216)';
         tab2.remove();
     })
     tabClose3.addEventListener('click', () => {
+        tabBtn3.checked = false;
+        ads.style.backgroundColor = 'rgb(228, 216, 216)';
         tab3.remove();
     })
     tabClose4.addEventListener('click', () => {
+        tabBtn4.checked = false;
+        ads.style.backgroundColor = 'rgb(228, 216, 216)';
         tab4.remove();
     })
     tabClose5.addEventListener('click', () => {
+        tabBtn5.checked = false;
+        ads.style.backgroundColor = 'rgb(228, 216, 216)';
         tab5.remove();
     })
 })
