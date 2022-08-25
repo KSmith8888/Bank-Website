cdf1c8cb39205caabbc15f4e84211bee4d19b383
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-08-24 17:47:07 -0700

    Update accounts.js
    Added logic to save each transaction in local storage so that it can be displayed within the account details page for each account. Added error handling for transfers.

--- JS/accounts.js
+++ JS/accounts.js
@@ -13,6 +13,9 @@ savingsBal.textContent = `$${savings}`;
 creditBal.textContent = `$${credit}`;
 let photoNum = 1;
 const loggedIn = document.getElementsByClassName("loggedIn");
+let newCheckingTransactions = [];
+let newSavingsTransactions = [];
+let newCreditTransactions = [];
 
 /*When user clicks button to transfer between accounts, event listener checks:
 -Which account it was transferred from
@@ -20,6 +23,7 @@ const loggedIn = document.getElementsByClassName("loggedIn");
 -The amount transferred
 Each account balance is updated*/
 tranBtnInternal.addEventListener('click', function() {
+    console.log(typeof(tranAmountInternal.value))
     /*Reduces the balance of the account that the transfer came from, checks to make sure the transfer amount is not larger than the balance*/
     if(tranFromInternal.value === 'Everyday Checking 4019' && parseInt(tranAmountInternal.value) < checking && parseInt(tranAmountInternal.value) > 0) {
         /*Increases the balance of the account the the transfer went to, or reduces the balance if it was a credit card*/
@@ -29,25 +33,59 @@ tranBtnInternal.addEventListener('click', function() {
             //Saves new balances in localStorage
             localStorage.setItem('savedSavingsBal', JSON.stringify(savings));
             localStorage.setItem('savedCheckingBal', JSON.stringify(checking));
-            } else if(tranToInternal.value === 'Silver Miles Credit Card 9124') {
+        /*
+            Pushes details of the tranfer to an array and sets a localStorage item specific to each account equal to the relevant array. This allows the appropriate transactions to appear within the account details page of each account.
+        */
+            newSavingsTransactions.push(`Transfer from Everyday Checking 4019: $${tranAmountInternal.value}`);
+            localStorage.setItem('newSavingsTransaction', JSON.stringify(newSavingsTransactions));
+            newCheckingTransactions.push(`Transfer to Reward Savings 8530: $${tranAmountInternal.value}`);
+            localStorage.setItem('newCheckingTransaction', JSON.stringify(newCheckingTransactions));
+            tranAmountInternal.value = '';
+        } else if(tranToInternal.value === 'Silver Miles Credit Card 9124') {
             credit -= parseInt(tranAmountInternal.value);
             checking -= parseInt(tranAmountInternal.value);
             localStorage.setItem('savedCreditBal', JSON.stringify(credit));
             localStorage.setItem('savedCheckingBal', JSON.stringify(checking));
-            }
+            newCreditTransactions.push(`Transfer from Everyday Checking 4019: $${tranAmountInternal.value}`);
+            localStorage.setItem('newCreditTransaction', JSON.stringify(newCreditTransactions));
+            newCheckingTransactions.push(`Transfer to Silver Miles Credit Card 9124: $${tranAmountInternal.value}`);
+            localStorage.setItem('newCheckingTransaction', JSON.stringify(newCheckingTransactions));
+            tranAmountInternal.value = '';
+        //Error handling if user attempts to transfer to and from the same account or does not select an account in either field
+        } else if(tranToInternal.value === 'Everyday Checking 4019') {
+            alert("You cannot transfer from and to the same account.");
+        } else if(tranToInternal.value === 'Transfer To:') {
+            alert('Please specify which account you would like to transfer to.');
+        }
     } else if(tranFromInternal.value === 'Rewards Savings 8530' && parseInt(tranAmountInternal.value) < savings && parseInt(tranAmountInternal.value) > 0) {
         if(tranToInternal.value === 'Everyday Checking 4019') {
             checking += parseInt(tranAmountInternal.value);
             savings -= parseInt(tranAmountInternal.value);
             localStorage.setItem('savedSavingsBal', JSON.stringify(savings));
             localStorage.setItem('savedCheckingBal', JSON.stringify(checking));
+            newSavingsTransactions.push(`Transfer to Everyday Checking 4019: $${tranAmountInternal.value}`);
+            localStorage.setItem('newSavingsTransaction', JSON.stringify(newSavingsTransactions));
+            newCheckingTransactions.push(`Transfer from Reward Savings 8530: $${tranAmountInternal.value}`);
+            localStorage.setItem('newCheckingTransaction', JSON.stringify(newCheckingTransactions));
+            tranAmountInternal.value = '';
         } else if(tranToInternal.value === 'Silver Miles Credit Card 9124' && credit - parseInt(tranAmountInternal.value) > 0) {
             credit -= parseInt(tranAmountInternal.value);
             savings -= parseInt(tranAmountInternal.value);
             localStorage.setItem('savedSavingsBal', JSON.stringify(savings));
             localStorage.setItem('savedCreditBal', JSON.stringify(credit));
-            }
-    } 
+            newCreditTransactions.push(`Transfer from Reward Savings 8530: $${tranAmountInternal.value}`);
+            localStorage.setItem('newCreditTransaction', JSON.stringify(newCreditTransactions));
+            newSavingsTransactions.push(`Transfer to Silver Miles Credit Card 9124: $${tranAmountInternal.value}`);
+            localStorage.setItem('newCreditTransaction', JSON.stringify(newCreditTransactions));
+            tranAmountInternal.value = '';
+        } else if(tranToInternal.value === 'Rewards Savings 8530') {
+            alert("You cannot transfer from and to the same account.");
+        } else if(tranToInternal.value === 'Transfer To:') {
+            alert('Please specify which account you would like to transfer to.');
+        }
+    } else if(tranFromInternal.value === 'Transfer From:') {
+        alert('Please specify which account you would like to transfer from.');
+    }
     //Updates html to reflect new balances after transfer
     checkingBal.innerText = `$${checking}`;
     savingsBal.innerText = `$${savings}`;
