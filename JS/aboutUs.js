2d500bc7f96293ffdcf8d9d46dabebf4d8809ae9
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-09-16 18:24:49 -0700

    Reorganized logic into global file

--- JS/aboutUs.js
+++ JS/aboutUs.js
@@ -1,16 +1,6 @@
 'use strict';
 
-const loggedIn = document.getElementsByClassName("loggedIn");
 const ourBoard = document.getElementById('ourBoard');
-const settingsForm = document.getElementById('settingsForm');
-const closeDialogBtn = document.getElementById('closeDialogBtn');
-const currentUserInput = document.getElementById('currentUserInput');
-const changeUserInput = document.getElementById('changeUserInput');
-const confirmUserInput = document.getElementById('confirmUserInput');
-const currentPassInput = document.getElementById('currentPassInput');
-const changePassInput = document.getElementById('changePassInput');
-const confirmPassInput = document.getElementById('confirmPassInput');
-const updateStatus = document.getElementById('updateStatus');
 const ourLocations = document.getElementById('ourLocations');
 
 function loadStaticUsers() {
@@ -99,77 +89,8 @@ function displayLocations() {
 }
 displayLocations()
 
-if(JSON.parse(localStorage.getItem('userId')) !== null && JSON.parse(localStorage.getItem('loggedIn')) !== null) {
-    for(let instance of loggedIn) {
-        instance.textContent = JSON.parse(localStorage.getItem('userId'));
-    }
-}
-
+//Sends user to homepage after clicking log out button
 document.querySelector('.logOutBtn').addEventListener('click', () => {
     localStorage.removeItem('loggedIn');
     location.href = './index.html';
-});
-
-document.querySelector('.settingsBtn').addEventListener('click', () => {
-    settingsDialog.showModal();
-});
-
-closeDialogBtn.addEventListener('click', () => {
-    settingsDialog.close();
-});
-
-settingsForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let updatedUser = false;
-    let updatedPass = false;
-    if(JSON.parse(localStorage.getItem('userId')) === currentUserInput.value && localStorage.getItem('userId') !== null) {
-        if(changeUserInput.value === confirmUserInput.value && changeUserInput.value !== '') {
-            localStorage.setItem('userId', JSON.stringify(changeUserInput.value));
-            updatedUser = true;
-        } else {
-            alert('Please make sure that both new User ID inputs match.');
-            currentUserInput.focus();
-        }
-    } else {
-        if(currentUserInput.value !== '') {
-            alert('Current User ID input does not match saved value or online account does not exist. If this is your first time using online banking, enter the User ID and Password you would like to use in the login section.');
-            currentUserInput.focus();
-        }
-    }
-    if(JSON.parse(localStorage.getItem('password')) === currentPassInput.value && localStorage.getItem('password') !== null) {
-        if(changePassInput.value === confirmPassInput.value && changePassInput.value !== '') {
-            localStorage.setItem('password', JSON.stringify(changePassInput.value));
-            updatedPass = true;
-        } else {
-            alert('Please make sure that both new Password inputs match.');
-            currentPassInput.focus();
-        }
-    } else {
-        if(currentPassInput.value !== '') {
-            alert('Current Password input does not match saved value or online account does not exist. If this is your first time using online banking, enter the User ID and Password you would like to use in the login section.');
-            currentPassInput.focus();
-        }
-    }
-    currentUserInput.value = '';
-    changeUserInput.value = '';
-    confirmUserInput.value = '';
-    changePassInput.value = '';
-    confirmPassInput.value = '';
-    currentPassInput.value = '';
-    if(updatedUser && !updatedPass) {
-        updateStatus.textContent = 'Success! User ID updated.';
-        setTimeout(() => {
-            location.reload(); 
-        }, 1500);
-    } else if(!updatedUser && updatedPass) {
-        updateStatus.textContent = 'Success! Password updated.';
-        setTimeout(() => {
-            location.reload(); 
-        }, 1500);
-    } else if(updatedUser && updatedPass) {
-        updateStatus.textContent = 'Success! User ID and Password updated.';
-        setTimeout(() => {
-            location.reload(); 
-        }, 1500);
-    }
 });
